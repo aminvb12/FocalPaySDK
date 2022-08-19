f36cfd321e4dd315098bc465d58ad01c7511e707
Amin Kazemi <32799160+aminvb12@users.noreply.github.com>
2022-08-19 19:57:40 +0430

    feat(SDKCore): broadcast QR data between native and web layer
    feat(SDKCore): broadcast QR data between native and web layer

--- Sources/Views/SwiftUIView.swift
+++ Sources/Views/SwiftUIView.swift
@@ -14,18 +14,26 @@ import FCUUID
 
 
 public struct SwiftUIView : View {
-
-   
     
-    public init() {
+    @Binding var qrcodeData: String
+
+    public init(qrcodeData: Binding<String>) {
+        _qrcodeData = qrcodeData
         PortalsRegistrationManager.shared.register(key: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIzMjI1OTAifQ.WjIrG3xRG9HnRbCQHd7xTINq4pWIkMHt-exeTuFEYFD7RzW1qQo1RahyxP_DdDJU5EsgsESCm6vUPKpHLnkkjLogQp_wiG6HqkcKj6odqt-P4Xe-38oKihXC4PScilAfwOgS9AKTh1LQ7EA5n2wmXLfip7DnjI3xPUkWt1Wo4B_vUWo571bDgdi6He_Kpztt8q1B_TBJDBilEXOBXdxWFAgftSZM4Yrn-u3Kb92-Itu2FpcIqR6w2k_kovRXd3g0RJHB5k_tzC1om9E2k1aUw76yaO30noCQnv4qx1ISugjeXEXFDML10W8uAY4lTreENXFiBdL6ux8_UoF077RFgw")
-      }
+        
+ 
+    }
+    
+    
+    func broadcastQRData (topic:String, data: Any){
+        IonicPortals.IONPortalsPubSub.publish(message: data, topic: topic)
+    }
     
 
     
     public var body: some View {
       
-        PortalView(portal: .init(name: "webapp", startDir: "focalpayPortal",bundle: .module, initialContext:  ["url": "?data=;;ci,1,624d83c313a2ae13989790a8", "deviceID": FCUUID.uuidForDevice()]))
+        PortalView(portal: .init(name: "webapp", startDir: "focalpayPortal",bundle: .module, initialContext:  ["url": qrcodeData, "deviceID": FCUUID.uuidForDevice()]))
     
      }
 }
