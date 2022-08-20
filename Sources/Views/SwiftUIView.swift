b7e21ed9f697d4bdd605637e84b22c6434108ec0
Amin Kazemi <32799160+aminvb12@users.noreply.github.com>
2022-08-20 19:55:57 +0430

    feat(Core Module): implement act on qr method
    feat(Core Module): implement act on qr method

--- Sources/Views/SwiftUIView.swift
+++ Sources/Views/SwiftUIView.swift
@@ -27,31 +27,51 @@ class MonitorChanges: ObservableObject {
 
 public struct SDKPackage : View {
     
-    @Binding var qrcodeData: String
+    
+    @Binding var userID: String
+//    @Binding var qrcodeData: String
     @Binding var callbackURL: String
+
+    @State var qrcodeData: String = ""
+    @State var authToken: String = ""
     
     @ObservedObject var listen =  MonitorChanges()
 
-    public init(qrcodeData: Binding<String>, callbackURL: Binding<String>) {
+    public init(userID: Binding<String> ,callbackURL: Binding<String>) {
         
-        _qrcodeData = qrcodeData
+        _userID = userID
         _callbackURL = callbackURL
         
+        
+        //login set token after API call
         
         PortalsRegistrationManager.shared.register(key: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIzMjI1OTAifQ.WjIrG3xRG9HnRbCQHd7xTINq4pWIkMHt-exeTuFEYFD7RzW1qQo1RahyxP_DdDJU5EsgsESCm6vUPKpHLnkkjLogQp_wiG6HqkcKj6odqt-P4Xe-38oKihXC4PScilAfwOgS9AKTh1LQ7EA5n2wmXLfip7DnjI3xPUkWt1Wo4B_vUWo571bDgdi6He_Kpztt8q1B_TBJDBilEXOBXdxWFAgftSZM4Yrn-u3Kb92-Itu2FpcIqR6w2k_kovRXd3g0RJHB5k_tzC1om9E2k1aUw76yaO30noCQnv4qx1ISugjeXEXFDML10W8uAY4lTreENXFiBdL6ux8_UoF077RFgw")
         
     }
     
     
+    
     public static func broadcastQRData (topic:String, data: Any) -> Void{
         IonicPortals.IONPortalsPubSub.publish(message: data, topic: topic)
     }
     
- 
+    
+    public func actOnQR(QR:String) {
+        //start pwa
+        self.qrcodeData = QR
+        
+    }
+    
     
     public var body: some View {
+
+        if qrcodeData.count > 0 && authToken.count > 0 {
+                PortalView(portal: .init(name: "webapp", startDir: "focalpayPortal",bundle: .module, initialContext:  ["url": qrcodeData, "deviceID": FCUUID.uuidForDevice(), "callbackURL": callbackURL, "token": authToken]))
+                
+                
+                //token
+            }
       
-        PortalView(portal: .init(name: "webapp", startDir: "focalpayPortal",bundle: .module, initialContext:  ["url": qrcodeData, "deviceID": FCUUID.uuidForDevice(), "callbackURL": callbackURL]))
-    
+
      }
 }
