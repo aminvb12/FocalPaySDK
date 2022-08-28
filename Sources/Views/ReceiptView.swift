914e24c0af5d79360f38da715891031db9e9926b
Amin Kazemi <32799160+aminvb12@users.noreply.github.com>
2022-08-28 21:38:42 +0430

    Factory Pattern
    Factory Pattern

--- Sources/Views/ReceiptView.swift
+++ Sources/Views/ReceiptView.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by amin on 8/17/22.
 //
@@ -12,23 +12,21 @@ import FCUUID
 
 
 
-public struct SDKPackage : View {
+public struct ReceiptView : View {
     
     @Binding var userID: String
-    @Binding var qrcodeData: String
-    @Binding var callbackURL: String
+
+    @Binding var storeID: String
+    @Binding var orderID: String
     
     
-    var callback : (_ type: String, _ value:Any?) -> Void
     
-    
-    public init(userID: Binding<String> ,callbackURL: Binding<String>, qrcodeData: Binding<String>, paymentCallbackHandler: @escaping (_ type:String,_ type: Any) -> Void) {
+    public init(userID: Binding<String> ,storeID: Binding<String> , orderID: Binding<String> ) {
         
         _userID = userID
-        _callbackURL = callbackURL
-        _qrcodeData = qrcodeData
-        
-        callback = paymentCallbackHandler
+        _storeID = storeID
+        _orderID = orderID
+
         
         
         PortalsRegistrationManager.shared.register(key: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIzMjI1OTAifQ.WjIrG3xRG9HnRbCQHd7xTINq4pWIkMHt-exeTuFEYFD7RzW1qQo1RahyxP_DdDJU5EsgsESCm6vUPKpHLnkkjLogQp_wiG6HqkcKj6odqt-P4Xe-38oKihXC4PScilAfwOgS9AKTh1LQ7EA5n2wmXLfip7DnjI3xPUkWt1Wo4B_vUWo571bDgdi6He_Kpztt8q1B_TBJDBilEXOBXdxWFAgftSZM4Yrn-u3Kb92-Itu2FpcIqR6w2k_kovRXd3g0RJHB5k_tzC1om9E2k1aUw76yaO30noCQnv4qx1ISugjeXEXFDML10W8uAY4lTreENXFiBdL6ux8_UoF077RFgw")
@@ -42,18 +40,14 @@ public struct SDKPackage : View {
     }
     
     
+    
     public var body: some View {
         
         VStack{
-            
-            PortalView(portal: .init(name: "webapp", startDir: "focalpayPortal",bundle: .module, initialContext:  ["url": qrcodeData, "deviceID": userID , "callbackURL": callbackURL]))
+            PortalView(portal: .init(name: "webapp", startDir: "focalpayPortal",bundle: .module, initialContext:  [ "storeId": storeID , "orderId": orderID ]))
 
         }
         
-        .onReceive(PortalsPubSub.publisher(for: "payment_parameter")
-            .data()){ orderParams in
-                callback("payment_parameter", orderParams!)
-            }
         
     }
     
---
+++ Sources/Views/SDKMain.swift
@@ -0,0 +1,40 @@
+//
+//  SwiftUIView 2.swift
+//  
+//
+//  Created by amin on 8/28/22.
+//
+
+import SwiftUI
+
+
+enum StateType {
+    case scan, receipt
+}
+
+
+
+struct Main: View {
+
+    @Binding var state: StateType 
+
+    @Binding var userID: String
+    @Binding var qrcodeData: String
+    @Binding var callbackURL: String
+
+
+    @Binding var storeID: String
+    @Binding var orderID: String
+    
+    var callback : (_ type: String, _ value:Any?) -> Void
+    
+    var body: some View {
+        VStack {
+            if state == .receipt {
+                ReceiptView(userID: $userID, storeID: $storeID, orderID: $orderID)
+            } else if state == .scan {
+                SDKPackage(userID: $userID, callbackURL: $callbackURL, qrcodeData: $qrcodeData, paymentCallbackHandler: callback)
+            }
+        }
+    }
+}
